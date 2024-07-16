06f857670235f581a1fd77dcfb014dc2f9540db7
dhaerieshan <67305891+dhaerieshan@users.noreply.github.com>
2024-07-16 21:50:46 +0530

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = 'wbr)%e!bdzhcwkv_#%)8j*)2pn53@7f=gv1pl@=)r4y#pme!84'
-SECRET_KEY = s.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'default_secret_key')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
