ce7893a457581829f89e1f922b1f064495a0174c
Dhaerieshan <dhaerieshan@gmail.com>
2024-07-22 12:59:05 +0530

    Fixed UI

--- myproject/settings.py
+++ myproject/settings.py
@@ -119,14 +119,18 @@ USE_I18N = True
 
 USE_TZ = True
 
-# settings.py
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'dhaerieshan@gmail.com'
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'default_password') # Or app-specific password if 2FA is enabled
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', 'default_password_if_not_set')
 
 
 
