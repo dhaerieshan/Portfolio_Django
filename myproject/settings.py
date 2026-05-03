f103b81b8308e2e5b19993f9a3739137e81c54bb
dhaerieshan <dhaerieshan@gmail.com>
2026-05-04 01:51:15 +0530

    revision

--- myproject/settings.py
+++ myproject/settings.py
@@ -148,8 +148,9 @@ USE_TZ = True
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 EMAIL_TIMEOUT = 10
 EMAIL_HOST_USER = 'dhaerieshan@gmail.com'
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
